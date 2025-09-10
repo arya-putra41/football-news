2f00e8605435ee5226e06772e5fbeb109d0914b1
arya.putra41 <aryakesit4@gmail.com>
2025-09-10 15:28:50 +0700

    Complete Tutorial 2

--- football_news/settings.py
+++ football_news/settings.py
@@ -33,6 +33,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "arya-putra41-footballnews.pbp.cs.ui.ac.id"]
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://arya-putra41-footballnews.pbp.cs.ui.ac.id/"
+]
 
 # Application definition
 
