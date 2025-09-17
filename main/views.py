26c437b7b580142f52f8e5d0c77913bfef6f8a3e
arya.putra41 <aryakesit4@gmail.com>
2025-09-17 15:14:37 +0700

    Complete Tutorial 3 (add accounts, authentication features, news filtering)

--- main/views.py
+++ main/views.py
@@ -1,17 +1,31 @@
+import datetime
+
 from django.shortcuts import render, redirect, get_object_or_404
 from main.forms import NewsForm
 from main.models import News
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 from django.core import serializers
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout, authenticate
+from django.contrib import messages
 
+@login_required(login_url="/login/")
 def show_main(request):
-    news_list = News.objects.all()
-    
+    filter_type = request.GET.get("filter", "all")
+
+    if filter_type == "all":
+        news_list = News.objects.all()
+    else:
+        news_list = News.objects.filter(user=request.user)
+
     context = {
         'npm' : '2406406300',
-        'name': 'Arya Putra Parikesit',
+        'name': request.user.username,
         'class': 'PBP C',
         'news_list': news_list,
+        'last_login': request.COOKIES.get('last_login', 'Never')
     }
 
     return render(request, "main.html", context)
@@ -19,13 +33,19 @@ def show_main(request):
 def create_news(request):
     form = NewsForm(request.POST or None)
 
-    if form.is_valid() and request.method == "POST":
-        form.save()
+    if form.is_valid() and request.method == 'POST':
+        news_entry = form.save(commit = False)
+        news_entry.user = request.user
+        news_entry.save()
         return redirect('main:show_main')
-    
-    context = {'form':form}
+
+    context = {
+        'form': form
+    }
+
     return render(request, "create_news.html", context)
 
+@login_required(login_url="/login/")
 def show_news(request, id):
     news = get_object_or_404(News, pk=id)
     news.increment_views()
@@ -35,6 +55,40 @@ def show_news(request, id):
     }
 
     return render(request, "news_detail.html", context)
+
+def register(request):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Selamat! Akun anda telah dibuat.")
+            return redirect("main:login")
+        
+    context = {"form": form}
+    return render(request, "register.html", context)
+
+def login_user(request):
+   if request.method == 'POST':
+      form = AuthenticationForm(data=request.POST)
+
+      if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            response = HttpResponseRedirect(reverse("main:show_main"))
+            response.set_cookie('last_login', str(datetime.datetime.now()))
+            return response
+   else:
+      form = AuthenticationForm(request)
+   context = {'form': form}
+   return render(request, 'login.html', context)
+
+def logout_user(request):
+    logout(request)
+    response = HttpResponseRedirect(reverse('main:login'))
+    response.delete_cookie('last_login')
+    return response
 
 def show_xml(request):
     news_list = News.objects.all()
