c9cf6c2e7c6100083a70cf95daf7e75770385f53
arya.putra41 <aryakesit4@gmail.com>
2025-09-24 16:21:43 +0700

    Complete Tutorial 4 (add edit and delete options, css styling with Tailwind)

--- main/views.py
+++ main/views.py
@@ -11,6 +11,7 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 
+# Function to show main page
 @login_required(login_url="/login/")
 def show_main(request):
     filter_type = request.GET.get("filter", "all")
@@ -30,6 +31,7 @@ def show_main(request):
 
     return render(request, "main.html", context)
 
+# Function to create new article
 def create_news(request):
     form = NewsForm(request.POST or None)
 
@@ -45,6 +47,7 @@ def create_news(request):
 
     return render(request, "create_news.html", context)
 
+# Function to show full news article
 @login_required(login_url="/login/")
 def show_news(request, id):
     news = get_object_or_404(News, pk=id)
@@ -56,6 +59,27 @@ def show_news(request, id):
 
     return render(request, "news_detail.html", context)
 
+# Function to edit news article (if user is author)
+def edit_news(request, id):
+    news = get_object_or_404(News, pk=id)
+    form = NewsForm(request.POST or None, instance=news)
+    if form.is_valid() and request.method == "POST":
+        form.save()
+        return redirect('main:show_main')
+
+    context = {
+        'form': form
+    }
+
+    return render(request, "edit_news.html", context)
+
+# Function to delete article (if user is author)
+def delete_news(request, id):
+    news = get_object_or_404(News, pk=id)
+    news.delete()
+    return HttpResponseRedirect(reverse('main:show_main'))
+
+# Function to show and process registration form
 def register(request):
     form = UserCreationForm()
 
@@ -63,12 +87,13 @@ def register(request):
         form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
-            messages.success(request, "Selamat! Akun anda telah dibuat.")
+            messages.success(request, "Account created successfully. \n Please log in with your new credentials.")
             return redirect("main:login")
         
     context = {"form": form}
     return render(request, "register.html", context)
 
+# Function to display and process login form
 def login_user(request):
    if request.method == 'POST':
       form = AuthenticationForm(data=request.POST)
@@ -84,22 +109,22 @@ def login_user(request):
    context = {'form': form}
    return render(request, 'login.html', context)
 
+# Function to log out user
 def logout_user(request):
     logout(request)
     response = HttpResponseRedirect(reverse('main:login'))
     response.delete_cookie('last_login')
     return response
 
+# Data delivery functions
 def show_xml(request):
     news_list = News.objects.all()
     xml_data = serializers.serialize("xml", news_list)
     return HttpResponse(xml_data, content_type="application/xml")
-
 def show_json(request):
     news_list = News.objects.all()
     json_data = serializers.serialize("json", news_list)
     return HttpResponse(json_data, content_type="application/json")
-
 def show_xml_by_id(request, news_id):
    try:
        news_item = News.objects.filter(pk=news_id)
@@ -107,7 +132,6 @@ def show_xml_by_id(request, news_id):
        return HttpResponse(xml_data, content_type="application/xml")
    except News.DoesNotExist:
        return HttpResponse(status=404)
-
 def show_json_by_id(request, news_id):
    try:
        news_item = News.objects.get(pk=news_id)
